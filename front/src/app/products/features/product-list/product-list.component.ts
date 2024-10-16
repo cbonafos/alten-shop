70ce0039791e5292f46511d5c739e4b081726439
cbonafos <c@email.fr>
2024-10-16 15:44:14 +0200

    feat(product-list): more details in item list

--- front/src/app/products/features/product-list/product-list.component.ts
+++ front/src/app/products/features/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from "@angular/common";
 import { Component, OnInit, inject, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { Product } from "app/products/data-access/product.model";
 import { ProductsService } from "app/products/data-access/products.service";
 import { ProductFormComponent } from "app/products/ui/product-form/product-form.component";
@@ -6,6 +8,8 @@ import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { DataViewModule } from 'primeng/dataview';
 import { DialogModule } from 'primeng/dialog';
+import { RatingModule } from 'primeng/rating';
+import { TagModule } from 'primeng/tag';
 
 const emptyProduct: Product = {
   id: 0,
@@ -29,7 +33,7 @@ const emptyProduct: Product = {
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.scss"],
   standalone: true,
-  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent],
+  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent, RatingModule, FormsModule, TagModule, CommonModule],
 })
 export class ProductListComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
@@ -76,4 +80,26 @@ export class ProductListComponent implements OnInit {
   private closeDialog() {
     this.isDialogVisible = false;
   }
+
+public getStatusInfo(product: Product): { severity: any, text: string } {
+  switch (product.inventoryStatus) {
+    case 'INSTOCK':
+      return { severity: 'success', text: 'In Stock' };
+    case 'LOWSTOCK':
+      return { severity: 'warning', text: 'Low Stock' };
+    case 'OUTOFSTOCK':
+      return { severity: 'danger', text: 'Out of Stock' };
+    default:
+      return { severity: 'info', text: 'Out of Stock' };
+  }
+}
+
+public getSeverity(product: Product): any {
+  return this.getStatusInfo(product).severity;
+}
+
+public getStockStatusText(product: Product): string {
+  return this.getStatusInfo(product).text;
+}
+
 }
